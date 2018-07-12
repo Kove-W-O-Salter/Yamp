7e0c9f35487940fd82587a75ee42a85443d17d7d
Kove W. Ochre-Salter <kove.w.o.salter>
2018-07-12 17:59:13 +1000

    Update style

--- src/Data/Yamp.hs
+++ src/Data/Yamp.hs
@@ -4,107 +4,108 @@
 --
 
 module Data.Yamp where
+
+--
+-- For Alternative.
+--
+import Control.Applicative
+
+--
+-- The Stream which a Parser
+-- parses.
+--
+type Stream
+    = String
+
+--
+-- The Parser type, which is just
+-- a state transformer where the
+-- state is a Stream.
+--
+newtype Parser a =
+    Parser (Stream -> [(a,Stream)])
+
+--
+-- Allowing Parser to be mapped,
+-- since I need it to be a Monad.
+--
+instance Functor Parser where
     --
-    -- For Alternative.
+    -- fmap :: (a -> b) -> Parser a -> Parser b
     --
-    import Control.Applicative
+    fmap f px = Parser (\stream0 ->
+        case apply px stream0 of
+            []            -> []
+            [(x,stream1)] -> [(f x,stream1)])
+
+--
+-- Allowing non-Parser functions
+-- to be applied to Parsers, since
+-- I need it to be a Monad.
+--
+instance Applicative Parser where
+    --
+    -- pure :: a -> Parser a
+    --
+    pure x = Parser (\stream0 ->
+        [(x,stream0)])
 
     --
-    -- The Stream which a Parser
-    -- parses.
+    -- (<*>) :: Parser (a -> b) -> Parser a -> Parser b
     --
-    type Stream
-        = String
+    pf <*> px = Parser (\stream0 ->
+        case apply pf stream0 of
+            []            -> []
+            [(f,stream1)] ->
+                case apply px stream1 of
+                    []            -> []
+                    [(x,stream2)] -> [(f x,stream2)])
+
+--
+-- Allowing Parser to be Monadic
+-- so that all the error handling
+-- and state is hidden from the
+-- user.
+--
+instance Monad Parser where
+    --
+    -- (>>=) :: Parser a -> (a -> Parser b) -> Parser b
+    --
+    px >>= fp = Parser (\stream0 ->
+        case apply px stream0 of
+            []            -> []
+            [(x,stream1)] -> apply (fp x) stream1)
+
+--
+--
+--
+instance Alternative Parser where
+    --
+    -- empty :: Parser a
+    --
+    empty = Parser (\stream0 ->
+        [])
 
     --
-    -- The Parser type, which is just
-    -- a state transformer where the
-    -- state is a Stream.
+    -- (<|>) :: Parser a -> Parser a -> Parser a
     --
-    newtype Parser a =
-        Parser (Stream -> [(a,Stream)])
+    p1 <|> p2 = Parser (\stream0 ->
+        case apply p1 stream0 of
+            []  -> apply p2 stream0
+            [r] -> [r])
 
     --
-    -- Allowing Parser to be mapped,
-    -- since I need it to be a Monad.
+    -- many :: Parser a -> [Parser a]
     --
-    instance Functor Parser where
-        --
-        -- fmap :: (a -> b) -> Parser a -> Parser b
-        --
-        fmap f px = Parser (\stream0 ->
-            case apply px stream0 of
-                []            -> []
-                [(x,stream1)] -> [(f x,stream1)])
+    many p = some p <|> pure []
 
     --
-    -- Allowing non-Parser functions
-    -- to be applied to Parsers, since
-    -- I need it to be a Monad.
+    -- some :: Parser a -> [Parser a]
     --
-    instance Applicative Parser where
-        --
-        -- pure :: a -> Parser a
-        --
-        pure x = Parser (\stream0 ->
-            [(x,stream0)])
+    some p = pure (:) <*> p <*> many p
 
-        --
-        -- (<*>) :: Parser (a -> b) -> Parser a -> Parser b
-        --
-        pf <*> px = Parser (\stream0 ->
-            case apply pf stream0 of
-                []            -> []
-                [(f,stream1)] ->
-                    case apply px stream1 of
-                        []            -> []
-                        [(x,stream2)] -> [(f x,stream2)])
-
-    --
-    -- Allowing Parser to be Monadic
-    -- so that all the error handling
-    -- and state is hidden from the
-    -- user.
-    --
-    instance Monad Parser where
-        --
-        -- (>>=) :: Parser a -> (a -> Parser b) -> Parser b
-        --
-        px >>= fp = Parser (\stream0 ->
-            case apply px stream0 of
-                []            -> []
-                [(x,stream1)] -> apply (fp x) stream1)
-
-    --
-    --
-    --
-    instance Alternative Parser where
-        --
-        -- empty :: Parser a
-        --
-        empty = Parser (\stream0 ->
-            [])
-
-        --
-        -- (<|>) :: Parser a -> Parser a -> Parser a
-        --
-        p1 <|> p2 = Parser (\stream0 ->
-            case apply p1 stream0 of
-                []  -> apply p2 stream0
-                [r] -> [r])
-
-        --
-        -- many :: Parser a -> [Parser a]
-        --
-        many p = some p <|> pure []
-
-        --
-        -- some :: Parser a -> [Parser a]
-        --
-        some p = pure (:) <*> p <*> many p
-
-    --
-    -- Remove a Parsers constructor.
-    --
-    apply            :: Parser a -> Stream -> [(a,Stream)]
-    apply (Parser f)  = f
+--
+-- Remove a Parsers constructor.
+--
+apply            :: Parser a -> Stream -> [(a,Stream)]
+apply (Parser f)  = f
